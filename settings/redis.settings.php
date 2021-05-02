56689c37e65498f62d80da5fe81fb0943e07c887
Malabya <malabya35@gmail.com>
2021-05-03 05:20:15 +0530

    Added Redis service command.

--- settings/redis.settings.php
+++ settings/redis.settings.php
@@ -5,20 +5,24 @@
  * Redis configuration.
  */
 
-// Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
-$settings['container_yamls'][] = 'modules/redis/example.services.yml';
-$conf['redis_client_host'] = 'cache';
+use Drupal\Core\Installer\InstallerKernel;
 
-//PhpRedis should be build in the application container.
-$settings['redis.connection']['interface'] = 'PhpRedis';
-// Check variables if present else fallback to default.
-$settings['redis.connection']['host'] = $_ENV['CACHE_HOST'] ?? 'cache';
-$settings['redis.connection']['port'] = $_ENV['CACHE_PORT'] ?? '6379';
+if (!InstallerKernel::installationAttempted() && extension_loaded('redis')) {
+  // Include the Redis services.yml file. Adjust the path if you installed to a contrib or other subdirectory.
+  $settings['container_yamls'][] = 'modules/redis/example.services.yml';
+  $conf['redis_client_host'] = 'cache';
 
-$settings['redis_compress_length'] = 100;
-$settings['redis_compress_level'] = 1;
+  //PhpRedis should be build in the application container.
+  $settings['redis.connection']['interface'] = 'PhpRedis';
+  // Check variables if present else fallback to default.
+  $settings['redis.connection']['host'] = $_ENV['CACHE_HOST'] ?? 'cache';
+  $settings['redis.connection']['port'] = $_ENV['CACHE_PORT'] ?? '6379';
 
-$settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
-$settings['cache_prefix']['default'] = 'specbee-redis';
+  $settings['redis_compress_length'] = 100;
+  $settings['redis_compress_level'] = 1;
 
-$settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
+  $settings['cache']['default'] = 'cache.backend.redis'; // Use Redis as the default cache.
+  $settings['cache_prefix']['default'] = 'specbee-redis';
+
+  $settings['cache']['bins']['form'] = 'cache.backend.database'; // Use the database for forms
+}
--- src/Robo/Plugin/Commands/CacheServiceCommand.php
+++ src/Robo/Plugin/Commands/CacheServiceCommand.php
@@ -2,6 +2,7 @@
 
 namespace SbRoboTooling\Robo\Plugin\Commands;
 
+use Robo\Contract\VerbosityThresholdInterface;
 use Robo\Exception\TaskException;
 use Robo\Result;
 use Robo\Tasks;
@@ -23,12 +24,20 @@ class CacheServiceCommand extends Tasks
     public function initServiceCache()
     {
         $this->say('init:recipe-redis');
-        $this->io()->section('Adding the Drupal Redis module via composer.');
-        $this->taskComposerRequire()
-        ->dependency('drupal/redis')
-        ->ansi()
-        ->noInteraction()
+
+        $task = $this->taskExec('composer show -- | grep "redis"')
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_DEBUG)
         ->run();
+        if (!$task->wasSuccessful()) {
+            $this->io()->section('Adding the Drupal Redis module via composer.');
+            $this->taskComposerRequire()
+            ->dependency('drupal/redis')
+            ->ansi()
+            ->noInteraction()
+            ->run();
+        } else {
+            $this->io()->note("Redis is already added to composer.json. Skipping..");
+        }
 
         $this->say('Enabling Redis module..');
         $this->drush()
@@ -37,23 +46,31 @@ class CacheServiceCommand extends Tasks
         ->arg('--no-interaction')
         ->run();
 
-        $task = $this->taskWriteToFile($this->getDocroot() . '/' . $this->getConfigValue('drupal.webroot') . '/sites/default/settings.php')
-        ->append()
-        ->line('# Redis Configuration.')
-        ->line('$conf[\'redis_client_host\'] = \'cache\';')
-        ->line('require DRUPAL_ROOT . "/../vendor/specbee/robo-tooling/settings/redis.settings.php";')
+        $this->say('Checking if Redis is already configured');
+        $task = $this->taskExec('drush rq | egrep "Redis|OK"')
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_DEBUG)
         ->run();
-        if (!$task->wasSuccessful()) {
-            throw new TaskException($task, "Could not udpate settings.php");
+        if ($task->wasSuccessful()) {
+            $this->io()->note('Redis is already installed and configured. Skipping...');
+        } else {
+            $task = $this->taskWriteToFile($this->getDocroot() . '/' . $this->getConfigValue('drupal.webroot') . '/sites/default/settings.php')
+            ->append()
+            ->line('# Redis Configuration.')
+            ->line('$conf[\'redis_client_host\'] = \'cache\';')
+            ->line('require DRUPAL_ROOT . "/../vendor/specbee/robo-tooling/settings/redis.settings.php";')
+            ->run();
+            if (!$task->wasSuccessful()) {
+                throw new TaskException($task, "Could not udpate settings.php");
+            }
         }
 
-        $confirm = $this->io()->confirm('Do you want to update Landofile to add the redis service?', true);
-        if (!$confirm) {
-            return Result::cancelled();
-        }
         $landoFileConfig = Yaml::parse(file_get_contents($this->getDocroot() . '/.lando.yml', 128));
         $this->say('Checking if there is cache service is setup.');
         if (!array_key_exists('cache', $landoFileConfig['services'])) {
+            $confirm = $this->io()->confirm('Do you want to update Lando to add the `cache` service for Redis?', true);
+            if (!$confirm) {
+                return Result::cancelled();
+            }
             $landoFileConfig['services']['cache'] = [
             'type' => 'redis:4.0',
             'portforward' => true,
@@ -65,7 +82,7 @@ class CacheServiceCommand extends Tasks
 
             file_put_contents($this->getDocroot() . '/.lando.yml', Yaml::dump($landoFileConfig, 5, 2));
             $this->io()->note('Lando configurations are updated with cache service.');
-            $this->io()->note('Do a `lando rebuild` for the change to take effect.');
+            $this->io()->warning('Do a `lando rebuild` for the change to take effect.');
         } else {
             $this->io()->note('Cache service is already added to Lando configuration. Skipping..');
         }
