22e27d7b5c8dd3523f63fbb2738228a76857c86c
Malabya <malabya35@gmail.com>
2022-03-19 02:01:53 +0530

    Fix PHP version replacement.

--- src/Robo/Plugin/Commands/InitCommands.php
+++ src/Robo/Plugin/Commands/InitCommands.php
@@ -219,6 +219,7 @@ class InitCommands extends Tasks
         $task = $this->taskReplaceInFile($landoFile)
         ->from('${PHP_VERSION}')
         ->to($this->getConfigValue('project.php'))
+        ->taskReplaceInFile($landoFile)
         ->from('${PROJECT_NAME}')
         ->to($this->getConfigValue('project.machine_name'))
         ->taskReplaceInFile($landoFile)
