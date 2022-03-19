52e27e89199437eeb3447a9723fcc33cc6553c1d
Malabya <malabya35@gmail.com>
2022-03-20 04:22:08 +0530

    Added command for Bitbucket pipeline setup.

--- src/Robo/Plugin/Commands/CiServiceCommand.php
+++ src/Robo/Plugin/Commands/CiServiceCommand.php
@@ -21,31 +21,66 @@ class CiServiceCommand extends Tasks
      *
      * @command service:init:ci:circle
      */
-    public function initServiceCiCircle()
+    public function serviceInitCiCircle()
     {
         $docroot = $this->getDocroot();
         $this->say('Setting up CI/CD using CircleCI.');
 
         if (file_exists($docroot . '/.circleci/config.yml')) {
-            $this->info('CircleCI config file exists.', true);
+            $this->info('Existing CircleCI configuration found.', true);
             return;
         }
+
         $source = $docroot . "/vendor/specbee/robo-tooling/ci/";
-        $dest = $docroot . "/.circleci/";
+        $dest = $docroot . "/.circleci";
 
         $task = $this->taskFilesystemStack()
         ->mkdir($dest)
-        ->mkdir($docroot . '/ci/.circleci')
-        ->copy($source . 'circleci/config.yml', $dest . 'config.yml')
-        ->copy($source . 'deploy.sh', $docroot . '/ci/.circleci/deploy.sh')
+        ->mkdir($docroot . '/scripts/.circleci')
+        ->copy($source . 'circleci/config.yml', $dest . '/config.yml')
+        ->copy($source . 'deploy.sh', $docroot . '/scripts/.circleci/deploy.sh')
         ->stopOnFail()
         ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
         ->run();
 
         if (!$task->wasSuccessful()) {
-            throw new TaskException($task, "Could not initialize Travis CI configuration.");
+            throw new TaskException($task, "Could not initialize CircleCI configuration.");
         }
 
-        $this->success("A pre-configured CircleCI was copied to your repository root.");
+        $this->success("A pre-configured CircleCI was copied to your repository.");
+    }
+
+    /**
+     * Setup Bitbucket Pipelines.
+     *
+     * @command service:init:ci:bitbucket-pipelines
+     */
+    public function serviceInitCiBitbucketPipelines()
+    {
+        $docroot = $this->getDocroot();
+        $this->say('Setting up CI/CD using Bitbucket Pipelines.');
+
+        if (file_exists($docroot . '/.bitbucket-pipelines.yml')) {
+            $this->info('Existing Bitbucket Pipeline configuration found.', true);
+            return;
+        }
+
+        $pipelineSource = $docroot . "/vendor/specbee/robo-tooling/ci/bitbucket/bitbucket-pipelines.yml";
+        $behatConfig = $docroot . "/vendor/specbee/robo-tooling/config/ci.behat.yml";
+        $dest = $docroot . "/scripts/.bitbucket";
+
+        $task = $this->taskFilesystemStack()
+        ->copy($pipelineSource, $docroot)
+        ->copy($behatConfig, $docroot . "/config/ci.behat.yml")
+        ->mkdir($dest)
+        ->copy($docroot . "/vendor/specbee/robo-tooling/ci/bitbucket/run", $docroot . "/scripts/run")
+        ->copy($docroot . "/vendor/specbee/robo-tooling/ci/bitbucket/deploy", $docroot . "/scripts/deploy")
+        ->run();
+
+        if (!$task->wasSuccessful()) {
+            throw new TaskException($task, "Could not initialize Bitbucket Pipeline configuration.");
+        }
+
+        $this->success("A pre-configured bitbucket-pipelines.yml was copied to your repository root.");
     }
 }
