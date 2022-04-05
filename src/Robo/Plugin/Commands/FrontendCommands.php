d3ea29370432752425f6eb6051a0bb4c77ce4194
Malabya <malabya35@gmail.com>
2022-04-05 17:29:35 +0530

    Added commands for frontend.

---
+++ src/Robo/Plugin/Commands/FrontendCommands.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Specbee\DevSuite\Robo\Plugin\Commands;
+
+use Robo\Exception\TaskException;
+use Robo\Result;
+use Robo\Tasks;
+use Specbee\DevSuite\Robo\Traits\IO;
+use Specbee\DevSuite\Robo\Traits\UtilityTrait;
+
+/**
+ * Defines the commands in the drupal:* namespace.
+ */
+class FrontendCommands extends Tasks
+{
+    use UtilityTrait;
+    use IO;
+
+    /**
+     * Build the frontend assets.
+     *
+     * @command theme:build
+     *
+     * @return Robo\Result
+     */
+    public function themeBuild()
+    {
+        $this->say('theme:build');
+
+        // Return early if theme is not found.
+        if (!file_exists($this->getConfigValue('drupal.theme.path'))) {
+            $this->warning('No theme found at ' . $this->getConfigValue('drupal.theme.path'));
+            return;
+        }
+
+
+        if (!empty($this->getConfigValue('drupal.theme.build'))) {
+            $task = $this->taskExec($this->getConfigValue('drupal.theme.build'))
+            ->dir($this->getConfigValue('drupal.theme.path'))
+            ->run();
+        }
+
+        if (!$task->wasSuccessful()) {
+            throw new TaskException($task, "Failed to build frontend assets!");
+        }
+
+        return $task;
+    }
+
+    /**
+     * Build the frontend assets.
+     *
+     * @command theme:compile
+     *
+     * @return Robo\Result
+     */
+    public function themeCompile()
+    {
+        $this->say('drupal:install');
+
+        // Return early if theme is not found.
+        if (!file_exists($this->getConfigValue('drupal.theme.path'))) {
+            $this->warning('No theme found at ' . $this->getConfigValue('drupal.theme.path'));
+            return;
+        }
+
+
+        if (!empty($this->getConfigValue('drupal.theme.compile'))) {
+            $task = $this->taskExec($this->getConfigValue('drupal.theme.compile'))
+            ->dir($this->getConfigValue('drupal.theme.path'))
+            ->run();
+        }
+
+        if (!$task->wasSuccessful()) {
+            throw new TaskException($task, "Failed to compile frontend assets!");
+        }
+
+        return $task;
+    }
+}
