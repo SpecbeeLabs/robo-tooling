e986501c14c64c748dca335c14c72268e4f632b7
Malabya <malabya35@gmail.com>
2023-09-20 16:41:57 +0530

    New deployment command to build artifact.

--- src/Robo/Plugin/Commands/DeployCommand.php
+++ src/Robo/Plugin/Commands/DeployCommand.php
@@ -2,10 +2,12 @@
 
 namespace Specbee\DevSuite\Robo\Plugin\Commands;
 
+use Robo\Contract\VerbosityThresholdInterface;
 use Robo\Exception\TaskException;
 use Robo\Tasks;
 use Specbee\DevSuite\Robo\Traits\IO;
 use Specbee\DevSuite\Robo\Traits\UtilityTrait;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Defines command deploy code.
@@ -15,13 +17,162 @@ class DeployCommand extends Tasks
     use UtilityTrait;
     use IO;
 
+    protected $branchName;
+
     /**
-     * RoboFile constructor.
+     * Builds the artifact and pushes to git.remotes.
+     *
+     * @command artifact:deploy
      */
-    public function __construct()
+    public function deploy($branch)
     {
-        // Treat this command like bash -e and exit as soon as there's a failure.
-        $this->stopOnFail();
+
+        if (empty($this->getConfigValue('project.git.remote'))) {
+            $this->info("The built artifact will not be pushed since the git remote configuration is not added.");
+        }
+
+        $this->branchName = $branch . "-build";
+        $this->prepareArtifact();
+        $this->sanitizeArtifact();
+        $this->addGitRemote();
+        $this->mergeUpstreamChanges();
+        $this->push();
+    }
+
+    /**
+     * Prepare the Artifact.
+     */
+    protected function prepareArtifact()
+    {
+        $this->say("Preparing the artifact...");
+        $composerBuildTask = $this->taskComposerInstall()
+        ->noDev()
+        ->optimizeAutoloader()
+        ->ansi()
+        ->run();
+
+        if (!$composerBuildTask->wasSuccessful()) {
+            $this->toggleMaintenanceMode(0);
+            return new TaskException($composerBuildTask, "Deployment failed. Check the logs for more information");
+        }
+
+        // Build frontend.
+        $this->buildTheme();
+    }
+
+    /**
+     * Sanitize the artifact.
+     */
+    protected function sanitizeArtifact()
+    {
+        $docroot = $this->getDocroot();
+        $this->say("Sanitizing the artifact...");
+        $this->taskExecStack()
+        ->exec("find '{$docroot}/vendor' -type d -name '.git' -exec rm -fr \\{\\} \\+")
+        ->exec("find '{$docroot}/docroot' -type d -name '.git' -exec rm -fr \\{\\} \\+")
+        ->stopOnFail()
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+        ->run();
+
+        $taskFilesystemStack = $this->taskFilesystemStack()
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE);
+
+        $finder = new Finder();
+        $files = $finder
+        ->in($docroot)
+        ->files()
+        ->name('CHANGELOG.txt');
+
+        foreach ($files->getIterator() as $item) {
+            $taskFilesystemStack->remove($item->getRealPath());
+        }
+
+        $finder = new Finder();
+        $files = $finder
+        ->in($docroot . '/' . $this->getConfigValue('drupal.webroot') . '/core')
+        ->files()
+        ->name('*.txt');
+
+        foreach ($files->getIterator() as $item) {
+            $taskFilesystemStack->remove($item->getRealPath());
+        }
+
+        $this->say("Removing .txt files...");
+        $taskFilesystemStack->run();
+    }
+
+    /**
+     * Adds a single remote to the /deploy repository.
+     */
+    protected function addGitRemote()
+    {
+        $git_remote = $this->getConfigValue('project.git.remote');
+        if (empty($git_remote)) {
+            $this->info("git.remotes is empty. Please define the git.remote in robo.yml.");
+        }
+      // Generate an md5 sum of the remote URL to use as remote name.
+        $this->say("Adding git remote:" . $this->getConfigValue('project.git.remote'));
+        $this->taskExecStack()
+        ->stopOnFail()
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+        ->dir($this->getDocroot())
+        ->exec("git remote add upstream $git_remote")
+        ->run();
+        $this->say("Added git remote:" . $this->getConfigValue('project.git.remote'));
+    }
+
+    /**
+     * Merges upstream changes into deploy branch.
+     */
+    protected function mergeUpstreamChanges()
+    {
+        $this->say("Merging upstream changes into local artifact...");
+        $task = $this->taskGitStack()
+        ->stopOnFail(false)
+        ->dir($this->getDocroot())
+        ->exec("fetch upstream {$this->branchName}")
+        ->exec("merge upstream/{$this->branchName}")
+        ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+        ->run();
+    }
+
+    /**
+     * Push the artifact to build repository.
+     */
+    protected function push()
+    {
+        $this->say("Committing artifact to <comment>{$this->branchName}</comment>...");
+
+        // Force add all dependencies.
+        chdir($this->getDocroot());
+        $webroot = $this->getDocroot() . '/' . $this->getConfigValue('drupal.webroot');
+        $dirs = [
+            'vendor',
+            $webroot . '/core',
+            $webroot . '/modules/contrib',
+            $webroot . '/themes/contrib',
+            $webroot . '/profiles/contrib',
+            $webroot . '/libraries',
+            $this->getConfigValue('drupal.theme.path')
+        ];
+        $this->taskGitStack()
+        ->exec("add -f " . implode(" ", $dirs))
+        ->run();
+
+        $this->taskGitStack()->exec('prune')->run();
+
+        // Commit the new changes.
+        $commit = trim(shell_exec("git rev-parse HEAD"));
+        $message = trim(shell_exec("git log -1 --pretty=%B"));
+        $task = $this->taskGitStack()
+        ->commit("Commit:" . $commit . ": " . $message)
+        ->push('upstream', $this->branchName)
+        ->run();
+
+
+        if (!$task->wasSuccessful()) {
+            throw new TaskException($task, "Failed to commit deployment artifact!");
+        }
     }
 
     /**
@@ -29,7 +180,7 @@ class DeployCommand extends Tasks
      *
      * @command fetch:remote:branch
      */
-    public function fetchRemoteBranch($remote, $branch)
+    public function fetchRemoteBranch($branch, $remote = 'origin')
     {
         $this->say('Remove any untracked file just for safety and fetch the remote changes.');
         $this->taskGitStack()
@@ -37,54 +188,26 @@ class DeployCommand extends Tasks
         ->checkout('.')
         ->exec('clean -f -d')
         ->checkout($branch)
-        ->exec('fetch ' . $remote . ' ' . $branch)
-        ->exec('rebase ' . $remote . '/' . $branch)
+        ->exec("fetch $remote {$this->branchName}")
+        ->exec("merge $remote/{$this->branchName}")
         ->run();
     }
 
     /**
-     * Run deployment commands.
+     * Run site updates after code deployment.
      *
-     * @command deploy
+     * @command artifact:update:drupal
      */
-    public function deploy()
+    public function siteUpdate()
     {
-        // Install composer dependencies with --no-dev flag.
-        $this->taskComposerInstall()
-        ->noDev()
-        ->noInteraction()
-        ->ansi()
-        ->run();
-
-        // Build frontend assets
-        $this->buildTheme();
-
+        $this->say("Deploying updates to " . $this->getConfigValue('project.human_name') . "...");
+        $this->say("Putting the site on maintenance mode");
+        $this->toggleMaintenanceMode(1);
         // Check site status to debug in case of failed deployment.
         $this->drush()
         ->args('core:status')
         ->option('ansi')
         ->run();
-
-        // Set the system UUID for importing configurations.
-        $this->cacheRebuild();
-        $this->drush()
-        ->arg('config:set')
-        ->arg('system.site')
-        ->arg('uuid')
-        ->arg($this->getExportedSiteUuid())
-        ->option('ansi')
-        ->option('no-interaction')
-        ->run();
-
-        // Put the site in maintenance mode in case of a Production deployment.
-        $this->say('Putting the site in manintenance mode');
-        $this->drush()
-        ->args('state:set')
-        ->args('system.maintenance_mode')
-        ->args('1')
-        ->option('ansi')
-        ->run();
-        $this->warning("Site is now offline");
 
         // Run the drush deploy command which runs the below commands.
         /**
@@ -102,7 +225,7 @@ class DeployCommand extends Tasks
         ->run();
 
         if (!$task->wasSuccessful()) {
-            $this->disableMaintenance();
+            $this->toggleMaintenanceMode(0);
             return new TaskException($task, "Deployment failed. Check the logs for more information");
         }
 
@@ -118,6 +241,10 @@ class DeployCommand extends Tasks
         ->option('no-interaction')
         ->run();
 
+        if (!$task->wasSuccessful) {
+            return new TaskException($task, "Deployment failed. Check the logs for more information");
+        }
+
         # Run cron.
         $this->drush()
         ->args('core-cron')
@@ -127,20 +254,7 @@ class DeployCommand extends Tasks
         # Clear the cache.
         $this->cacheRebuild();
 
-        #Disable maintenance mode for production deployments.
-        $this->disableMaintenance();
-
-        $this->success("🚀 Deployment completed. Site is now online. 🚀");
-    }
-
-    private function disableMaintenance()
-    {
-        $this->say('Disabling maintenance mode.');
-        $this->drush()
-        ->args('state:set')
-        ->args('system.maintenance_mode')
-        ->args('0')
-        ->option('ansi')
-        ->run();
+        $this->say("Removing the site from maintenance mode");
+        $this->toggleMaintenanceMode(1);
     }
 }
